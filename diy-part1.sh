cc7c1f9e7072bbeb9f474c68e9e3c2dcdf901186
ytalm <75474524+ytalm@users.noreply.github.com>
2024-02-08 21:45:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git Whitecolor https://github.com/Whitestorys/openwrt-extra-packages.git' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
 #sed -i '$a src-git 281677160 https://github.com/281677160/openwrt-package' feeds.conf.default
 #sed -i '$a src-git Aibx https://github.com/Aibx/OpenWRT-Packages' feeds.conf.default
 #sed -i '$a src-git ytalm https://github.com/ytalm/ytalm-packages' feeds.conf.default
