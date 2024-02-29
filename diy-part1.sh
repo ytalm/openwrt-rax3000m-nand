b4f0b641124d0f49a3acac4ca44053ac1210f8bb
ytalm <75474524+ytalm@users.noreply.github.com>
2024-02-29 17:35:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,8 +21,8 @@ sed -i '$a src-git 281677160 https://github.com/281677160/openwrt-package' feeds
 sed -i '$a src-git haiibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 sed -i '$a src-git ytalm https://github.com/ytalm/my-packages' feeds.conf.default
 #sed -i '$a src-git Aibx https://github.com/Aibx/OpenWRT-Packages' feeds.conf.default
-#sed -i '$a src-git fw876 https://github.com/fw876/helloworld' feeds.conf.default
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git ipsec https://github.com/kenzok8/small-package/tree/main/luci-app-ipsec-server' feeds.conf.default
+sed -i '$a src-git pptp https://github.com/kenzok8/small-package/tree/main/luci-app-pptp-server' feeds.conf.default
 #sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
 
 #内核版本是会随着源码更新而改变的，在coolsnowwolf/lede的源码查看最好，以X86机型为例，源码的target/linux/x86文件夹可以看到有几个内核版本，x86文件夹里Makefile可以查看源码正在使用内核版本
@@ -35,8 +35,6 @@ sed -i '$a src-git ytalm https://github.com/ytalm/my-packages' feeds.conf.defaul
 # git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom  #透明主题
 # git clone https://github.com/jerrykuku/lua-maxminddb     #git lua-maxminddb 依赖
 # git clone https://github.com/jerrykuku/luci-app-vssr
-git clone https://github.com/kenzok8/small-package/tree/main/luci-app-ipsec-server
-git clone https://github.com/kenzok8/small-package/tree/main/luci-app-pptp-server
 # git clone https://github.com/xiaorouji/openwrt-passwall2  #passwall出国软件
 
 # rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
