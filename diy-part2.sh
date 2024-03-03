b5cca7150fafb68b04dd092034ad9c2a7fd03582
ytalm <75474524+ytalm@users.noreply.github.com>
2024-03-03 20:36:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/172.16.10.1/g' ./package/base-files/files/bin/config_generate
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/OpenWrt-RAX3000M/g' ./package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/immortalwrt/g' ./package/base-files/files/bin/config_generate
 
 # Enable wifi
 # sed -i 's/.disabled=1/.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
